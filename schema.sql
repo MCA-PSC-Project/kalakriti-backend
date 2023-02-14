358f8271e4d40baec7254e9ae9ff699e60a386df
sheetal <sheetalkumarilife@gmail.com>
2023-02-14 21:46:59 +0530

    schrma.sql created

--- schema.sql
+++ schema.sql
@@ -3,6 +3,11 @@ BEGIN;
 CREATE type "media__type" as enum ('image', 'audio', 'video', 'doc', 'pdf');
 CREATE type "user__type" as enum ('customer', 'seller', 'admin', 'super_admin');
 CREATE type "gender__type" as enum ('male', 'female', 'other');
+CREATE type "order__status" as enum ('initiated', 'pending' ,'confirmed_by_seller', 'cancelled_by_seller',
+'cancelled_by_customer', 'dispatched', 'shipped', 'delivered', 'return_request', 'return_apporved',
+'returned', 'sold');
+CREATE type "payment__status" as enum ('failure', 'success', 'pending');
+CREATE type "approval__status" as enum ('pending', 'apporved', 'rejected');
 -----------------------TABLES-------------------------------------------
 CREATE TABLE "media"(
 	"id" int PRIMARY KEY,
@@ -79,21 +84,93 @@ CREATE TABLE "wishlists"(
 	FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL,
 	PRIMARY KEY(user_id,product_id)
 );
-CREATE TABLE "carts"(
-    "user_id" int PRIMARY KEY ,
-	"product_ids" int[],
-	"added_at" timestamptz NOT NULL,
-	"updated_at" timestamptz NOT NULL,
-	FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
-	FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL,
-);
+-- CREATE TABLE "carts"(
+--     "user_id" int PRIMARY KEY ,
+-- 	"product_ids" int[],
+-- 	"added_at" timestamptz NOT NULL,
+-- 	"updated_at" timestamptz NOT NULL,
+-- 	FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
+-- 	FOREIGN KEY("product_ids") references "products"("id") ON DELETE SET NULL
+-- );
 CREATE TABLE "banners"(
 	"id" serial PRIMARY KEY,
 	"media_id" int,
 	"redirect_type" varchar,
 	"redirect_url" varchar NOT NULL,
 	FOREIGN KEY("media_id") references "media"("id")
-)
+);
+CREATE TABLE "orders"(
+	"id" serial PRIMARY KEY,
+	"user_id" integer,
+	"address" varchar NOT NULL,
+    "city" varchar NOT NULL,
+    "state" varchar NOT NULL,
+	"district" varchar NOT NULL,
+	"pincode" varchar NOT NULL,
+	"country" varchar NOT NULL,
+	"phone" varchar NOT NULL,
+	"ordered_at" timestamptz NOT NULL,
+	"order_status" order__status,
+	"updated_at" timestamptz DEFAULT NULL,
+	"sub_total" numeric NOT NULL,
+	"discount" numeric NOT NULL DEFAULT 0,
+	"tax" numeric NOT NULL DEFAULT 0,
+	"grand_total" numeric NOT NULL,
+	FOREIGN KEY("user_id") references "users"("id") ON DELETE CASCADE
+);
+CREATE TABLE "ordered_products"(
+     "order_id" int,
+	 "product_id" int,
+     "SKU" varchar,
+	 "original_price" numeric NOT NULL,
+	 "offer_price" numeric NOT NULL,
+	 "discount" numeric NOT NULL,
+	 "tax" numeric NOT NULL,
+	 "quantity" float DEFAULT 1,
+	 FOREIGN KEY("order_id") references "orders"("id") ON DELETE SET NULL,
+	 FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL
+);
+CREATE TABLE "product_reviews"(
+   "product_id" int,
+   "SKU" varchar,
+   "user_id" int,
+   "rating" numeric(2,1),
+   "review" varchar,
+   "added_at" timestamptz NOT NULL,
+   "updated_at" timestamptz NOT NULL,
+   FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL,
+   FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
+   PRIMARY KEY(product_id,user_id)
+);
+CREATE TABLE "payments"(
+   "id" serial PRIMARY KEY,
+   "order_id" int,
+   "provider" varchar(50) NOT NULL,
+   "provider_order_id" varchar NOT NULL,
+   "provider_payment_id" varchar NOT NULL,
+   "payment_status" payment__status,
+   "added_at" timestamptz NOT NULL,
+   "updated_at" timestamptz NOT NULL,
+   FOREIGN KEY("order_id") references "orders"("id") ON DELETE SET NULL
+);
+CREATE TABLE "seller_applicant_forms"(
+    "id" serial PRIMARY KEY,
+	"name" varchar NOT NULL,
+	"email" varchar NOT NULL,
+	"mobile_no" varchar NOT NULL,
+	"reviewed" BOOLEAN DEFAULT false,
+	"added_at" timestamptz NOT NULL,
+	"updated_at" timestamptz NOT NULL,
+	"approval_status" approval__status DEFAULT 'pending',
+	"description" varchar,
+	"path" varchar DEFAULT ' '
+);
+CREATE TABLE "mobile_otp"(
+   "mobile_no" varchar(15) PRIMARY KEY,
+   "otp" varchar(6),
+   "expiry" int
+);
+
 ----- Indexes -----
 CREATE INDEX ON "users" ("email");
 CREATE INDEX ON "users" ("phone");
