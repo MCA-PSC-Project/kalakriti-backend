c15cd3d4b07412c9630f2ce6960b1e64a1f44821
Prashant Kumar <prashantbittu123@gmail.com>
2023-02-15 21:59:19 +0530

    upload image, video,file

--- app/resources/media.py
+++ app/resources/media.py
@@ -93,8 +93,108 @@ class UploadImage(Resource):
 
 
 class UploadVideo(Resource):
-    pass
+    @f_jwt.jwt_required()
+    def post(self):
+        if "file" not in request.files:
+            return "No user_file key in request.files"
+
+        source_file = request.files["file"]
+
+        if source_file.filename == "":
+            return "Please select a file"
+
+        if source_file:
+
+            source_filename = secure_filename(source_file.filename)
+            source_extension = os.path.splitext(source_filename)[1]
+            destination_filename = uuid4().hex + source_extension
+            app.logger.debug("destination file name= %s", destination_filename)
+            source_file.filename = destination_filename
+            path_name = upload_file_to_s3(
+                source_file, app.config["S3_BUCKET"])
+            # return str(output)
+            full_url = "{}/{}".format(app.config["S3_LOCATION"], path_name)
+            app.logger.debug(str(full_url))
+
+            current_time = datetime.now()
+            INSERT_MEDIA = '''INSERT INTO media(name, path, media_type, added_at)
+            VALUES(%s, %s, %s, %s) RETURNING id'''
+
+            # catch exception for invalid SQL statement
+            try:
+                # declare a cursor object from the connection
+                cursor = main.db_conn.cursor()
+                # app.logger.debug("cursor object: %s", cursor)
+
+                cursor.execute(INSERT_MEDIA, (source_filename,
+                               path_name, 'video', current_time,))
+                media_id = cursor.fetchone()[0]
+            except (Exception, psycopg2.Error) as err:
+                app.logger.debug(err)
+                abort(400, 'Bad Request')
+            finally:
+                cursor.close()
+            media_dict = {
+                "id": media_id,
+                "media_type": 'video',
+                "path": path_name,
+                "full_url": full_url
+            }
+            return media_dict, 201
+        else:
+            abort(404)
+
 
 
 class UploadFile(Resource):
-    pass
+    @f_jwt.jwt_required()
+    def post(self):
+        if "file" not in request.files:
+            return "No user_file key in request.files"
+
+        source_file = request.files["file"]
+
+        if source_file.filename == "":
+            return "Please select a file"
+
+        if source_file:
+
+            source_filename = secure_filename(source_file.filename)
+            source_extension = os.path.splitext(source_filename)[1]
+            destination_filename = uuid4().hex + source_extension
+            app.logger.debug("destination file name= %s", destination_filename)
+            source_file.filename = destination_filename
+            path_name = upload_file_to_s3(
+                source_file, app.config["S3_BUCKET"])
+            # return str(output)
+            full_url = "{}/{}".format(app.config["S3_LOCATION"], path_name)
+            app.logger.debug(str(full_url))
+
+            current_time = datetime.now()
+            INSERT_MEDIA = '''INSERT INTO media(name, path, media_type, added_at)
+            VALUES(%s, %s, %s, %s) RETURNING id'''
+
+            # catch exception for invalid SQL statement
+            try:
+                # declare a cursor object from the connection
+                cursor = main.db_conn.cursor()
+                # app.logger.debug("cursor object: %s", cursor)
+
+                cursor.execute(INSERT_MEDIA, (source_filename,
+                               path_name, 'file', current_time,))
+                media_id = cursor.fetchone()[0]
+            except (Exception, psycopg2.Error) as err:
+                app.logger.debug(err)
+                abort(400, 'Bad Request')
+            finally:
+                cursor.close()
+            media_dict = {
+                "id": media_id,
+                "media_type": 'file',
+                "path": path_name,
+                "full_url": full_url
+            }
+            return media_dict, 201
+        else:
+            abort(404)
+
