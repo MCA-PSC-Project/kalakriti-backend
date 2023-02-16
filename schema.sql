5f26ff5e18f075a6a06b1d026235ada46031a948
Prashant Kumar <prashantbittu123@gmail.com>
2023-02-16 16:27:29 +0530

    updated schema

--- schema.sql
+++ schema.sql
@@ -3,11 +3,22 @@ BEGIN;
 CREATE type "media__type" as enum ('image', 'audio', 'video', 'file');
 CREATE type "user__type" as enum ('customer', 'seller', 'admin', 'super_admin');
 CREATE type "gender__type" as enum ('male', 'female', 'other');
-CREATE type "order__status" as enum ('initiated', 'pending' ,'confirmed_by_seller', 'cancelled_by_seller',
-'cancelled_by_customer', 'dispatched', 'shipped', 'delivered', 'return_request', 'return_apporved',
-'returned', 'sold');
+CREATE type "order__status" as enum (
+	'initiated',
+	'pending',
+	'confirmed_by_seller',
+	'cancelled_by_seller',
+	'cancelled_by_customer',
+	'dispatched',
+	'shipped',
+	'delivered',
+	'return_request',
+	'return_apporved',
+	'returned',
+	'sold'
+);
 CREATE type "payment__status" as enum ('failure', 'success', 'pending');
-CREATE type "approval__status" as enum ('pending', 'apporved', 'rejected');
+CREATE type "approval__status" as enum ('pending', 'approved', 'rejected');
 -----------------------TABLES-------------------------------------------
 CREATE TABLE "media"(
 	"id" serial PRIMARY KEY,
@@ -28,7 +39,7 @@ CREATE TABLE "users"(
 	"gender" gender__type NOT NULL,
 	"added_at" timestamptz NOT NULL,
 	"updated_at" timestamptz DEFAULT NULL,
-	"dp_id" int,
+	"dp_id" int DEFAULT NULL,
 	"trash" boolean NOT NULL DEFAULT false,
 	"is_verified" boolean NOT NULL DEFAULT false,
 	"verified_at" timestamptz DEFAULT NULL,
@@ -39,26 +50,28 @@ CREATE TABLE "user_address"(
 	"id" serial PRIMARY KEY,
 	"user_id" integer,
 	"address" varchar NOT NULL,
-    "city" varchar NOT NULL,
-    "state" varchar NOT NULL,
+	"city" varchar NOT NULL,
+	"state" varchar NOT NULL,
 	"district" varchar NOT NULL,
-	"pincode" varchar NOT NULL,
 	"country" varchar NOT NULL,
+	"pincode" varchar NOT NULL,
 	"added_at" timestamptz NOT NULL,
 	"updated_at" timestamptz DEFAULT NULL,
 	FOREIGN KEY("user_id") references "users"("id") ON DELETE CASCADE
 );
 CREATE TABLE "categories"(
 	"id" serial PRIMARY KEY,
-	"name" varchar NOT NULL,
+	"name" varchar NOT NULL UNIQUE,
 	"added_at" timestamptz NOT NULL,
 	"updated_at" timestamptz DEFAULT NULL,
 	"added_by" int,
 	"cover_id" int,
 	"parent_id" int DEFAULT NULL,
-	FOREIGN KEY("added_by") references "users"("id") ON DELETE CASCADE,
-	FOREIGN KEY("parent_id") references "categories"("id"),
-	FOREIGN KEY("cover_id") references "media"("id") ON DELETE
+	FOREIGN KEY("added_by") references "users"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("parent_id") references "categories"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("cover_id") references "media"("id") ON DELETE
 	SET NULL
 );
 CREATE TABLE "products"(
@@ -67,32 +80,36 @@ CREATE TABLE "products"(
 	"description" varchar NOT NULL,
 	"category_id" int,
 	"subcategory_id" int,
-    "original_price" decimal(10,2) NOT NULL,
-	"offer_price" decimal(10,2) NOT NULL,
+	"original_price" decimal(10, 2) NOT NULL,
+	"offer_price" decimal(10, 2) NOT NULL,
 	"added_at" timestamptz NOT NULL,
 	"updated_at" timestamptz DEFAULT NULL,
 	"has_variants" BOOLEAN DEFAULT false,
 	"SKU" varchar UNIQUE,
 	"stock" int,
-	FOREIGN KEY("category_id") references "categories"("id") ON DELETE SET NULL,
-	FOREIGN KEY("subcategory_id") references "categories"("id") ON DELETE SET NULL
+	FOREIGN KEY("category_id") references "categories"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("subcategory_id") references "categories"("id") ON DELETE
+	SET NULL
 );
 CREATE TABLE "wishlists"(
-    "user_id" int ,
+	"user_id" int,
 	"product_id" int,
 	"added_at" timestamptz NOT NULL,
-	FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
-	FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL,
-	PRIMARY KEY(user_id,product_id)
+	PRIMARY KEY("user_id", "product_id"),
+	FOREIGN KEY("user_id") references "users"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("product_id") references "products"("id") ON DELETE
+	SET NULL
 );
--- CREATE TABLE "carts"(
---     "user_id" int PRIMARY KEY ,
--- 	"product_ids" int[],
--- 	"added_at" timestamptz NOT NULL,
--- 	"updated_at" timestamptz NOT NULL,
--- 	FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
--- 	FOREIGN KEY("product_ids") references "products"("id") ON DELETE SET NULL
--- );
+CREATE TABLE "carts"(
+	"user_id" int PRIMARY KEY,
+	"product_ids" int [],
+	"added_at" timestamptz NOT NULL,
+	"updated_at" timestamptz NOT NULL,
+	FOREIGN KEY("user_id") references "users"("id") ON DELETE
+	SET NULL
+);
 CREATE TABLE "banners"(
 	"id" serial PRIMARY KEY,
 	"media_id" int,
@@ -104,11 +121,11 @@ CREATE TABLE "orders"(
 	"id" serial PRIMARY KEY,
 	"user_id" integer,
 	"address" varchar NOT NULL,
-    "city" varchar NOT NULL,
-    "state" varchar NOT NULL,
+	"city" varchar NOT NULL,
+	"state" varchar NOT NULL,
 	"district" varchar NOT NULL,
-	"pincode" varchar NOT NULL,
 	"country" varchar NOT NULL,
+	"pincode" varchar NOT NULL,
 	"phone" varchar NOT NULL,
 	"ordered_at" timestamptz NOT NULL,
 	"order_status" order__status,
@@ -120,42 +137,47 @@ CREATE TABLE "orders"(
 	FOREIGN KEY("user_id") references "users"("id") ON DELETE CASCADE
 );
 CREATE TABLE "ordered_products"(
-     "order_id" int,
-	 "product_id" int,
-     "SKU" varchar,
-	 "original_price" numeric NOT NULL,
-	 "offer_price" numeric NOT NULL,
-	 "discount" numeric NOT NULL,
-	 "tax" numeric NOT NULL,
-	 "quantity" float DEFAULT 1,
-	 FOREIGN KEY("order_id") references "orders"("id") ON DELETE SET NULL,
-	 FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL
+	"order_id" int,
+	"product_id" int,
+	"SKU" varchar,
+	"original_price" numeric NOT NULL,
+	"offer_price" numeric NOT NULL,
+	"discount" numeric NOT NULL,
+	"tax" numeric NOT NULL,
+	"quantity" numeric DEFAULT 1,
+	FOREIGN KEY("order_id") references "orders"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("product_id") references "products"("id") ON DELETE
+	SET NULL
 );
 CREATE TABLE "product_reviews"(
-   "product_id" int,
-   "SKU" varchar,
-   "user_id" int,
-   "rating" numeric(2,1),
-   "review" varchar,
-   "added_at" timestamptz NOT NULL,
-   "updated_at" timestamptz NOT NULL,
-   FOREIGN KEY("product_id") references "products"("id") ON DELETE SET NULL,
-   FOREIGN KEY("user_id") references "users"("id") ON DELETE SET NULL,
-   PRIMARY KEY(product_id,user_id)
+	"product_id" int,
+	"SKU" varchar,
+	"user_id" int,
+	"rating" numeric(2, 1),
+	"review" varchar,
+	"added_at" timestamptz NOT NULL,
+	"updated_at" timestamptz NOT NULL,
+	PRIMARY KEY("product_id", "user_id"),
+	FOREIGN KEY("product_id") references "products"("id") ON DELETE
+	SET NULL,
+		FOREIGN KEY("user_id") references "users"("id") ON DELETE
+	SET NULL
 );
 CREATE TABLE "payments"(
-   "id" serial PRIMARY KEY,
-   "order_id" int,
-   "provider" varchar(50) NOT NULL,
-   "provider_order_id" varchar NOT NULL,
-   "provider_payment_id" varchar NOT NULL,
-   "payment_status" payment__status,
-   "added_at" timestamptz NOT NULL,
-   "updated_at" timestamptz NOT NULL,
-   FOREIGN KEY("order_id") references "orders"("id") ON DELETE SET NULL
+	"id" serial PRIMARY KEY,
+	"order_id" int,
+	"provider" varchar(50) NOT NULL,
+	"provider_order_id" varchar NOT NULL,
+	"provider_payment_id" varchar NOT NULL,
+	"payment_status" payment__status,
+	"added_at" timestamptz NOT NULL,
+	"updated_at" timestamptz NOT NULL,
+	FOREIGN KEY("order_id") references "orders"("id") ON DELETE
+	SET NULL
 );
 CREATE TABLE "seller_applicant_forms"(
-    "id" serial PRIMARY KEY,
+	"id" serial PRIMARY KEY,
 	"name" varchar NOT NULL,
 	"email" varchar NOT NULL,
 	"mobile_no" varchar NOT NULL,
@@ -164,14 +186,13 @@ CREATE TABLE "seller_applicant_forms"(
 	"updated_at" timestamptz NOT NULL,
 	"approval_status" approval__status DEFAULT 'pending',
 	"description" varchar,
-	"path" varchar DEFAULT ' '
+	"path" varchar DEFAULT ''
 );
 CREATE TABLE "mobile_otp"(
-   "mobile_no" varchar(15) PRIMARY KEY,
-   "otp" varchar(6),
-   "expiry" int
+	"mobile_no" varchar(15) PRIMARY KEY,
+	"otp" varchar(6) NOT NULL,
+	"expiry" int
 );
-
 ----- Indexes -----
 CREATE INDEX ON "users" ("email");
 CREATE INDEX ON "users" ("phone");
