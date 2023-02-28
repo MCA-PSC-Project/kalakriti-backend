d1492a2319ca016cec0d8ca6cb27000a8ba8e7d8
Prashant Kumar <prashantbittu123@gmail.com>
2023-02-28 10:10:15 +0530

    named tuple cursor in wishlists.py incomplete

--- app/resources/wishlists.py
+++ app/resources/wishlists.py
@@ -47,16 +47,21 @@ class Wishlists(Resource):
 
         wishlists_list = []
 
-        GET_WISHLISTS = '''SELECT product_id ,(SELECT product_name FROM products WHERE id = product_items.product_id),
-        product_variant_name ,"SKU" ,original_price, offer_price 
-        FROM product_items 
-        WHERE id IN
-        (SELECT product_item_id FROM wishlists WHERE user_id =%s )'''
+        GET_WISHLISTS = '''SELECT pi.product_id, pi.id AS product_item_id,
+        (SELECT p.product_name FROM products p WHERE p.id = pi.product_id),
+        pi.product_variant_name ,pi.original_price, pi.offer_price,
+        (SELECT v.variant AS variant FROM variants v WHERE v.id =
+        (SELECT vv.variant_id FROM variant_values vv WHERE vv.id = piv.variant_value_id)),
+        (SELECT vv.variant_value AS variant_value FROM variant_values vv WHERE vv.id = piv.variant_value_id) 
+        FROM product_items pi
+        JOIN product_item_values piv ON pi.id = piv.product_item_id
+        WHERE pi.id IN
+        (SELECT w.product_item_id FROM wishlists w WHERE w.user_id =%s)'''
 
         # catch exception for invalid SQL statement
         try:
             # declare a cursor object from the connection
-            cursor = app_globals.get_cursor()
+            cursor = app_globals.get_named_tuple_cursor()
             # # app.logger.debug("cursor object: %s", cursor)
             cursor.execute(GET_WISHLISTS, (user_id,))
             rows = cursor.fetchall()
@@ -64,16 +69,16 @@ class Wishlists(Resource):
                 return {}
             for row in rows:
                 wishlists_dict = {}
-                wishlists_dict['product_id'] = row[0]
-                wishlists_dict['product_name'] = row[1]
-                wishlists_dict['product_variant_name'] = row[2]
-                wishlists_dict['SKU'] = row[3]
-
+                wishlists_dict['product_id'] = row.product_id
+                wishlists_dict['product_item_id'] = row.product_item_id
+                wishlists_dict['product_name'] = row.product_name
+                wishlists_dict['product_variant_name'] = row.product_variant_name                
                 wishlists_dict.update(json.loads(
-                    json.dumps({'original_price': row[4]}, default=str)))
+                    json.dumps({'original_price': row.original_price}, default=str)))
                 wishlists_dict.update(json.loads(
-                    json.dumps({'offer_price': row[5]}, default=str)))
-
+                    json.dumps({'offer_price': row.offer_price}, default=str)))
+                wishlists_dict['variant'] = row.variant
+                wishlists_dict['variant_value'] = row.variant_value
                 wishlists_list.append(wishlists_dict)
         except (Exception, psycopg2.Error) as err:
             app.logger.debug(err)
