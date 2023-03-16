8bd9ea8c42bfc3d7c67fc1e866ddf1706215b88d
Prashant Kumar <prashantbittu123@gmail.com>
2023-03-16 12:30:19 +0530

    setupmfa auth fixed

--- app/resources/auth_mfa.py
+++ app/resources/auth_mfa.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+import json
 from flask import flash, redirect, render_template, request, abort, jsonify, url_for
 from flask_restful import Resource
 import flask_jwt_extended as f_jwt
@@ -19,22 +20,43 @@ class MFAStatus(Resource):
         user_type = claims['user_type']
         app.logger.debug("user_type= %s", user_type)
 
-        table_name = user_type+'s'
-        GET_USER_EMAIL = '''SELECT mfa_enabled FROM {} WHERE id= %s'''.format(
-            table_name)
+        mfa_dict = {}
         try:
             cursor = app_globals.get_named_tuple_cursor()
-            cursor.execute(GET_USER_EMAIL, (user_id,))
+
+            GET_MFA_STATUS = '''SELECT mfa_enabled FROM {} WHERE id= %s'''.format(
+                user_type+'s')
+            cursor.execute(GET_MFA_STATUS, (user_id,))
             row = cursor.fetchone()
             if row is None:
                 abort(400, 'Bad Request')
-            mfa_enabled = row.mfa_enabled
+            mfa_dict['mfa_enabled'] = row.mfa_enabled
+
+            GET_MFA_DETAILS = '''SELECT id AS mfa_id, mfa_type, is_default, added_at, updated_at 
+            FROM {0} WHERE {1} = %s'''.format(user_type+'s_mfa', user_type+'_id')
+            cursor.execute(GET_MFA_DETAILS, (user_id,))
+            rows = cursor.fetchall()
+            if not rows:
+                return mfa_dict
+            mfa_items_list = []
+            for row in rows:
+                mfa_item_dict = {}
+                mfa_item_dict['mfa_id'] = row.mfa_id
+                mfa_item_dict['mfa_type'] = row.mfa_type
+                mfa_item_dict['is_default'] = row.is_default
+                mfa_item_dict.update(json.loads(
+                    json.dumps({'added_at': row.added_at}, default=str)))
+                mfa_item_dict.update(json.loads(
+                    json.dumps({'updated_at': row.updated_at}, default=str)))
+                mfa_items_list.append(mfa_item_dict)
+            mfa_dict.update({"mfa_items": mfa_items_list})
         except (Exception, psycopg2.Error) as err:
             app.logger.debug(err)
             abort(400, 'Bad Request')
         finally:
             cursor.close()
-        return {"mfa_enabled": mfa_enabled}, 200
+        return mfa_dict, 200
+
 
 class TOTPAuthenticationSetup(Resource):
     # get secret key with provisioning uri
@@ -99,9 +121,9 @@ class TOTPAuthenticationSetup(Resource):
         if user_type == 'super_admin':
             user_type = 'admin'
 
-        GET_TOTP_SECRET_KEY = '''SELECT u.mfa_enabled, um.secret_key 
-        FROM {} u
-        JOIN {} um ON u.id = um.{} 
+        GET_TOTP_SECRET_KEY = '''SELECT u.mfa_enabled, um.id AS mfa_id, um.secret_key 
+        FROM {0} u
+        JOIN {1} um ON u.id = um.{2} AND um.mfa_type = 'totp'
         WHERE u.id= %s'''.format(
             user_type+'s', user_type+'s_mfa', user_type+'_id')
         try:
@@ -111,6 +133,7 @@ class TOTPAuthenticationSetup(Resource):
             if row is None:
                 abort(400, 'Bad Request')
             mfa_enabled = row.mfa_enabled
+            mfa_id = row.mfa_id
             totp_secret_key = row.secret_key
         except (Exception, psycopg2.Error) as err:
             app.logger.debug(err)
@@ -121,38 +144,57 @@ class TOTPAuthenticationSetup(Resource):
         if not totp_secret_key:
             abort(400, 'Bad Request')
 
-        is_verified = otp.verify_totp(totp_secret_key, provided_totp)
-        app.logger.debug("is_verified= %s", is_verified)
-        if not is_verified:
+        totp_matched = otp.verify_totp(totp_secret_key, provided_totp)
+        app.logger.debug("totp_matched= %s", totp_matched)
+        if not totp_matched:
             abort(400, 'Bad Request: Invalid totp')
 
         # for enabling mfa using totp
-        if not mfa_enabled:
-            if user_type == 'super_admin':
-                user_type = 'admin'
-            # TODO: Generate 10 digit backup key
-            backup_key = otp.generate_motp(otp_length=10)
-            app.logger.debug(backup_key)
+        if user_type == 'super_admin':
+            user_type = 'admin'
+        # Generate 10 digit backup key
+        backup_key = otp.generate_motp(otp_length=10)
+        app.logger.debug("backup_key= %s", backup_key)
 
-            hashed_backup_key = bcrypt.hashpw(
-                backup_key.encode('utf-8'), bcrypt.gensalt())
-            hashed_backup_key = hashed_backup_key.decode('utf-8')
+        hashed_backup_key = bcrypt.hashpw(
+            backup_key.encode('utf-8'), bcrypt.gensalt())
+        hashed_backup_key = hashed_backup_key.decode('utf-8')
 
-            UPDATE_MFA_ENABLED_HASHED_BACKUP_KEY = '''UPDATE {} SET mfa_enabled= %s, hashed_backup_key= %s,
-            updated_at= %s WHERE id= %s'''.format(user_type+'s')
-            try:
-                cursor = app_globals.get_cursor()
-                cursor.execute(UPDATE_MFA_ENABLED_HASHED_BACKUP_KEY,
-                               (True, hashed_backup_key, datetime.now(), user_id))
-            except (Exception, psycopg2.Error) as err:
-                app.logger.debug(err)
-                abort(400, 'Bad Request')
-            finally:
-                cursor.close()
-            # return "MFA successfully setup with backup key {backup_key}", 201
-            return {'mfa_totp_setup': 'success', 'backup_key': backup_key}, 201
+        UPDATE_MFA_ENABLED_HASHED_BACKUP_KEY = '''UPDATE {} SET mfa_enabled= %s, hashed_backup_key= %s,
+        updated_at= %s WHERE id= %s'''.format(user_type+'s')
+        try:
+            cursor = app_globals.get_cursor()
+            cursor.execute(UPDATE_MFA_ENABLED_HASHED_BACKUP_KEY,
+                            (True, hashed_backup_key, datetime.now(), user_id,))
+            if cursor.rowcount != 1:
+                abort(400, 'Bad Request: update {} row error'.format(
+                    user_type+'s'))
+        except (Exception, psycopg2.Error) as err:
+            app.logger.debug(err)
+            abort(400, 'Bad Request')
+        finally:
+            cursor.close()
 
-        # # if mfa is already enabled/login with mfa
+        MAKE_TOTP_MFA_DEFAULT = '''UPDATE {} SET is_default= %s, updated_at= %s 
+        WHERE {} = %s AND mfa_type='totp'
+        '''.format(user_type+'s_mfa', user_type+'_id')
+        try:
+            cursor = app_globals.get_cursor()
+            cursor.execute(MAKE_TOTP_MFA_DEFAULT,
+                            (True, datetime.now(), user_id))
+            if cursor.rowcount != 1:
+                app.logger.debug("could not set totp as default mfa")
+                # abort(400, 'Bad Request: update {} row error'.format(user_type+'s_mfa'))
+        except (Exception, psycopg2.Error) as err:
+            app.logger.debug(err)
+            abort(400, 'Bad Request')
+        finally:
+            cursor.close()
+        # return "MFA successfully setup with backup key {backup_key}", 201
+        return {'mfa_totp_setup': 'success', 'backup_key': backup_key}, 201
+
+        # if mfa is already enabled/login with mfa
+
         # access_token = f_jwt.create_access_token(
         #     identity=user_id, additional_claims={"user_type": user_type}, fresh=True)
         # refresh_token = f_jwt.create_refresh_token(
@@ -161,6 +203,7 @@ class TOTPAuthenticationSetup(Resource):
         #     'access_token': access_token,
         #     'refresh_token': refresh_token
         # }, 202
+
 
 class TOTPAuthentication(Resource):
     pass
