1524898239993cdd7e4b87dddf7f6604f921ffdb
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-03-30 19:23:41 +0530

    wishlists order by added_at using join lateral

--- app/resources/wishlists.py
+++ app/resources/wishlists.py
@@ -7,6 +7,7 @@ import flask_jwt_extended as f_jwt
 import json
 from flask import current_app as app
 
+
 class Wishlists(Resource):
     @f_jwt.jwt_required()
     def post(self):
@@ -35,16 +36,23 @@ class Wishlists(Resource):
         app.logger.debug("customer_id= %s", customer_id)
 
         wishlists_list = []
-        GET_WISHLISTS = '''SELECT pi.product_id, pi.id AS product_item_id,
-        (SELECT p.product_name FROM products p WHERE p.id = pi.product_id),
-        pi.product_variant_name ,pi.original_price, pi.offer_price,
-        (SELECT v.variant AS variant FROM variants v WHERE v.id =
-        (SELECT vv.variant_id FROM variant_values vv WHERE vv.id = piv.variant_value_id)),
-        (SELECT vv.variant_value AS variant_value FROM variant_values vv WHERE vv.id = piv.variant_value_id) 
-        FROM product_items pi
-        JOIN product_item_values piv ON pi.id = piv.product_item_id
-        WHERE pi.id IN
-        (SELECT w.product_item_id FROM wishlists w WHERE w.customer_id =%s)'''
+        GET_WISHLISTS = '''SELECT w.added_at,
+        temp.product_id, temp.product_name, temp.product_item_id,
+        temp.product_variant_name, temp.variant, temp.variant_value, temp.original_price, temp.offer_price
+        FROM wishlists w
+        JOIN LATERAL(
+            SELECT pi.product_id, pi.id AS product_item_id,
+            (SELECT p.product_name FROM products p WHERE p.id = pi.product_id),
+            pi.product_variant_name ,pi.original_price, pi.offer_price,
+            (SELECT v.variant AS variant FROM variants v WHERE v.id =
+            (SELECT vv.variant_id FROM variant_values vv WHERE vv.id = piv.variant_value_id)),
+            (SELECT vv.variant_value AS variant_value FROM variant_values vv WHERE vv.id = piv.variant_value_id) 
+            FROM product_items pi
+            JOIN product_item_values piv ON pi.id = piv.product_item_id
+            WHERE pi.id = w.product_item_id
+        ) AS temp ON TRUE
+        WHERE w.customer_id = %s 
+        ORDER BY w.added_at DESC'''
 
         try:
             cursor = app_globals.get_named_tuple_cursor()
@@ -54,6 +62,8 @@ class Wishlists(Resource):
                 return {}
             for row in rows:
                 wishlist_dict = {}
+                wishlist_dict.update(json.loads(
+                    json.dumps({'added_at': row.added_at}, default=str)))
                 wishlist_dict['product_id'] = row.product_id
                 wishlist_dict['product_name'] = row.product_name
                 product_item_dict = {}
