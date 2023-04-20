7ed831230dc84b7b30d0724ead95436dd0401118
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-04-20 23:10:54 +0530

    product item reviews partial fixes

--- app/resources/product_item_review.py
+++ app/resources/product_item_review.py
@@ -8,11 +8,11 @@ import json
 from flask import current_app as app
 
 
-class Product_item_review(Resource):
+class ProductItemReview(Resource):
     @f_jwt.jwt_required()
     def post(self):
-        user_id = f_jwt.get_jwt_identity()
-        app.logger.debug("user_id= %s", user_id)
+        customer_id = f_jwt.get_jwt_identity()
+        app.logger.debug("customer_id= %s", customer_id)
         claims = f_jwt.get_jwt()
         user_type = claims["user_type"]
         app.logger.debug("user_type= %s", user_type)
@@ -24,32 +24,25 @@ class Product_item_review(Resource):
 
         rating = data.get("rating", None)
         review = data.get("review", None)
-
         current_time = datetime.now()
-
-        # catch exception for invalid SQL statement
         try:
-            # declare a cursor object from the connection
             cursor = app_globals.get_cursor()
-            # # app.logger.debug("cursor object: %s", cursor)
             GET_PRODUCT_ITEM_ID = (
-                """SELECT product_item_id FROM order_items WHERE order_id = %s"""
+                """SELECT product_item_id FROM order_items WHERE id = %s"""
             )
             cursor.execute(GET_PRODUCT_ITEM_ID, (order_item_id,))
-
             row = cursor.fetchone()
             if not row:
                 app.logger.debug("product_item_id not found!")
                 app_globals.db_conn.rollback()
             product_item_id = row[0]
 
-            CREATE_REVIEW = """INSERT INTO product_item_reviews(user_id, order_item_id, product_item_id, rating, review, added_at)
+            CREATE_REVIEW = """INSERT INTO product_item_reviews(customer_id, order_item_id, product_item_id, rating, review, added_at)
             VALUES(%s, %s, %s, %s, %s, %s) RETURNING id"""
-
             cursor.execute(
                 CREATE_REVIEW,
                 (
-                    user_id,
+                    customer_id,
                     order_item_id,
                     product_item_id,
                     rating,
@@ -64,20 +57,18 @@ class Product_item_review(Resource):
         finally:
             cursor.close()
         return (
-            f"Review_id = {review_id} created sucessfully for product_item_id ={product_item_id}",
+            f"Review id = {review_id} created sucessfully for product item id = {product_item_id}",
             201,
         )
 
     @f_jwt.jwt_required()
     def get(self, product_id):
         reviews_list = []
-
         GET_REVIEWS = """SELECT id,customer_id, rating, review , added_at , updated_at FROM product_item_reviews 
                          WHERE product_item_id IN (SELECT id FROM product_items WHERE product_id =%s)"""
 
         try:
             cursor = app_globals.get_named_tuple_cursor()
-
             cursor.execute(GET_REVIEWS, (product_id,))
             rows = cursor.fetchall()
             if not rows:
